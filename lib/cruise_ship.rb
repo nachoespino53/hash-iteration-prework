5eb3b417c2a3d2c7ba54f790bfad7e8cce6deab6
Ignacio R Espino <irespino@capozios.com>
2018-09-06 20:22:23 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -9,7 +9,7 @@
 def select_winner(passengers)
   passengers.each do |key, value|
     winner = ""
-    if key == suite_a: && value.start_with?("A")
+    if key == :suite_a && value.start_with?("A")
       winner = value
     end
   end
